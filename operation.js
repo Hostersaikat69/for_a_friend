16e9819469ed6feac3e30c215ba859a802f7e34f
Hostersaikat69 <hostersaikat69@gmail.com>
2024-12-30 12:16:10 +0530

    Add files via upload

---
+++ operation.js
@@ -0,0 +1,11 @@
+const learnBtn = document.querySelector(".learnMore");
+
+learnBtn.addEventListener("click", ()=> {
+    window.open("https://www.pw.live/");
+})
+
+const installBtn = document.querySelector(".installBtn");
+
+installBtn.addEventListener("click", ()=> {
+    window.open("https://www.physicswallahforpc.in/");
+})
